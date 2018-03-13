9ad122e76c07f6391018ba5550160b705f09b042
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-13 18:35:06 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
   attr_accessor :id, :name, :type, :hp, :db
 
-  def initialize (pokemon,hp=nil)
+  def initialize (pokemon)
     @id = pokemon[:id]
     @name = pokemon[:name]
     @type = pokemon[:type]
