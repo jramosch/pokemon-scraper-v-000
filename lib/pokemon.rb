1c0fbaba9507aa6bdde54dbe25906fee5094de74
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-13 15:28:34 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -22,7 +22,7 @@ class Pokemon
   end
 
   def alter_hp(hp, db)
-    db.execute("UPDATE pokemon SET hp = ? WHERE hp = 60", hp)
+    db.execute("UPDATE pokemon SET hp = ? WHERE name = ", hp)
   end
   
 end
