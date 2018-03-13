3501f0893b880a814101baef60f1f693322ebde3
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-13 14:49:58 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize (pokemon)
+    @id = pokemon[:id]
+    @name = pokemon[:name]
+    @type = pokemon[:type]
+    @db = pokemon[:db]
+  end
+
+  def self.save (name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?",id)
+    pokemon = pokemon.flatten
+    new_pokemon = {}
+    new_pokemon = {id: pokemon[0], name: pokemon[1], type: pokemon[2], db: "#{db}"}
+    self.new(new_pokemon)
+  end
+
+  def alter_hp(hp, db)
+    
+  end
+  
 end
